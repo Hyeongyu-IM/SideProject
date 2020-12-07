5e42bc590f9260e3bbbd07ed314a0ee8b8bf0926
Hyeongyu-IM <64323969+Hyeongyu-IM@users.noreply.github.com>
2020-12-07 15:25:37 +0900

    let's begin to make basicApp

---
+++ BasicWeatherApp iOS/BasicWeatherApp iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicWeatherApp iOS
+//
+//  Created by 임현규 on 2020/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
