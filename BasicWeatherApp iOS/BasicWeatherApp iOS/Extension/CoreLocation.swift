6a89ace70553c3c5c9bfa0a0014cff6a7efe17d9
임현규 <hyeongyuim@imhyeongyuui-Macmini.local>
2021-01-18 20:01:44 +0900

    save

--- BasicWeatherApp iOS/BasicWeatherApp iOS/Extension/CoreLocation.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CoreLocation.swift
-//  BasicWeatherApp iOS
-//
-//  Created by 임현규 on 2021/01/02.
-//
-
-import Foundation
--- BasicWeatherApp iOS/BasicWeatherApp iOS/Utilities/CurrentLocation.swift
+++
@@ -1,7 +0,0 @@
-//
-//  CurrentLocation.swift
-//  BasicWeatherApp iOS
-//
-//  Created by 임현규 on 2020/12/12.
-//
-
